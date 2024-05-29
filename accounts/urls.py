2d0511ef6406234455d3f42807e7b8f4706b0eb3
Emmanuel K. Mensah <ekmpizarro@gmail.com>
2024-05-29 19:45:42 +0000

    Modified authentications templates; modified urls for password reset and password change;updated static files;

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,33 +1,20 @@
-from django.urls import path, re_path
+from django.urls import path
 from accounts import views as accounts_views
 from django.contrib.auth import views as auth_views
-
 
 urlpatterns = [
     path('signup/', accounts_views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'), 
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('settings/accounts/', accounts_views.UserUpdateView.as_view(), name='my_account'),
-    
-    path('reset/', 
-        auth_views.PasswordResetView.as_view(
-            template_name='registration/password_reset.html',
-            email_template_name='registration/password_reset_email.html',
-            subject_template_name='registration/password_reset_subject.txt'
-        ), 
-        name='password_reset'),
-    path('reset/done/', 
-        auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), 
-        name='password_reset_done'),
-    re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 
-        auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), 
-        name='password_reset_confirm'),
-    path('reset/complete/', 
-        auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), 
-        name='password_reset_complete'),
 
-    path('settings/password/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
-        name='password_change'),
-    path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
-        name='password_change_done'),
+    # Password Reset URLs
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='authentication/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='authentication/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='authentication/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='authentication/password_reset_complete.html'), name='password_reset_complete'),
+    
+    # Password Change URLs
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='authentication/password_change_form.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='authentication/password_change_done.html'), name='password_change_done'),
 ]
--- myproject/settings.py
+++ myproject/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from decouple import config, Csv
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@oew&))zfc@o2)^c)wcvzqn!eghlq!bl0)-#ywtq_-5n4l88u&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
